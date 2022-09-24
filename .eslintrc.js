99f0fbac804c1d1f27f50e0f935424d50283d992
aaron staes <aaron.staes@gmail.com>
2022-09-24 14:00:35 +0200

    Prettier and ESLint setup

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  // ...
+  extends: ["eslint:recommended", "plugin:astro/recommended"],
+  // ...
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {},
+    },
+    {
+      files: ["*.ts"],
+      parser: "@typescript-eslint/parser",
+      extends: ["plugin:@typescript-eslint/recommended"],
+      rules: {
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
+        ],
+        "@typescript-eslint/no-non-null-assertion": "off",
+      },
+    },
+  ],
+};
